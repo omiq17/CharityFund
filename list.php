79659fd8c78d09702638da63197226341f486efe
omiq17 <omiq17@gmail.com>
2016-11-14 12:33:03 +0600

    Added Make Paid option

--- list.php
+++ list.php
@@ -3,12 +3,26 @@
  ?>
  <!--start Content-->
          <div class="col-md-9 content">
+           <?php
+           //if user dont check
+           if(isset($_SESSION['form']) && !empty($_SESSION['form'])){
+             echo "<p style='color:red;'>
+              Please check order to make them paid.</p>";
+              unset($_SESSION['form']);
+           }
+           else if(isset($_SESSION['success']) && !empty($_SESSION['success'])){
+             echo "<p style='color:green;'>
+              Successfully made orders paid.</p>";
+              unset($_SESSION['success']);
+           }
+            ?>
              <div>
-             <form class="form-inline" method="post" action="delproduct.php" >
+             <form class="form-inline" method="post" action="makePaid.php" >
              <table  id="example" class="table table-striped">
                  <thead>
                    <tr>
                     <!-- <th><input type="checkbox"  id="checkAll"/></th> -->
+                     <th> </th>
                      <th>Doner Name</th>
                      <th>Ammount</th>
                      <th>Purpose</th>
@@ -25,7 +39,8 @@
                            while($row=mysqli_fetch_array($result)){
                        ?>
                                <tr>
-                                   <form action="" method="POST">
+                                      <td><input type="checkbox" name="selected[]"
+                                          value="<?php echo $row['d_id']?>"/></td>
                                        <td><?php echo $row['d_name']?></td>
                                        <td><?php echo $row['d_amount']?></td>
                                        <td><?php echo $row['d_purpose']?></td>
@@ -33,13 +48,13 @@
                                        <td><?php echo $row['d_addr']?></td>
                                        <td><?php echo $row['d_cell']?></td>
                                </tr>
-                                   </form>
                            <?php
                            }
                            ?>
                </tbody>
-
                </table>
+               <input type="submit" class="btn btn-primary btn-md"
+                      value="Make Paid" name="makePaid">
              </form>
              </div>
          </div>
---
+++ makePaid.php
@@ -0,0 +1,31 @@
+<?php
+require_once "header.php";
+if (isset($_POST['makePaid'])){
+    if( isset($_POST['selected']) ){
+        $id=$_POST['selected'];
+        // print_r ($id);
+        // die();
+        //$N = count($id);
+        foreach ($id as $item){
+            $query = " UPDATE `doner` SET `d_pay`='Paid' WHERE `doner`.`d_id`='$item' ";
+            $result = mysqli_query($link, $query);
+            if (!$result) {
+                // $_SESSION['del']=$item;
+                // header("location: list.php");
+                echo "error";
+                die();
+                }
+           }
+        //successfully paid all orders
+        $_SESSION['success']='added';
+        header("location: list.php");
+    }
+
+    //if nothing is checked
+    else{
+        $_SESSION['form']='error';
+        header("location: list.php");
+    }
+    //end of if/else
+}
+?>
